9e0524212aedcd3a673d1d94a171923e803ef3eb
Nouman Nadeem <160357491+NoumanNadeem123@users.noreply.github.com>
2024-03-15 00:57:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var transportation = ["civic", "BMW", "mercedese", "prado"];
+for (var i = 0; i < transportation.length; i++) {
+    console.log("i would like to go Pakistan tour on own a " + transportation[i]);
+}
